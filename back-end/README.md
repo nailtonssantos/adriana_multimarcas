df9089bb935262293f8793ed89de74e8c65d335b
NailtonSSantos <devnailton@gmail.com>
2023-02-18 10:32:31 -0400

    Alteração na estruturta e integração do Front-End

---
+++ back-end/README.md
@@ -0,0 +1,3 @@
+﻿# adriana_multimarcas
+
+Criação de um E-commerce com SpringBoot
--- back-end/src/main/java/br/com/nailton/adriana_multimarcas/controllers/ProdutoController.java
+++ back-end/src/main/java/br/com/nailton/adriana_multimarcas/controllers/ProdutoController.java
@@ -2,6 +2,7 @@ package br.com.nailton.adriana_multimarcas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.nailton.adriana_multimarcas.model.ResponseModel;
 import br.com.nailton.adriana_multimarcas.services.ProdutoService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 
     @Autowired
