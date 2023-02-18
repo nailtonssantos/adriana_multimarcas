5c343b08058a4d639466bb1ba3d83147f9a64cd9
NailtonSSantos <devnailton@gmail.com>
2023-02-17 22:02:11 -0400

    10Serviço para validar e cadastrar o produto

--- src/main/java/br/com/nailton/adriana_multimarcas/services/ProdutoService.java
+++ src/main/java/br/com/nailton/adriana_multimarcas/services/ProdutoService.java
@@ -1,9 +1,12 @@
 package br.com.nailton.adriana_multimarcas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.nailton.adriana_multimarcas.model.ProdutoModel;
+import br.com.nailton.adriana_multimarcas.model.ResponseModel;
 import br.com.nailton.adriana_multimarcas.repository.ProdutoRepository;
 
 @Service
@@ -12,9 +15,33 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository pr;
 
+    @Autowired
+    private ResponseModel rm;
+
     // Método para listar todos os produtos
     public Iterable<ProdutoModel> listar() {
         return pr.findAll();
+    };
+
+    // Método para cadastrar produtos
+    public ResponseEntity<?> cadastrar(ProdutoModel pm) {
+
+        if (pm.getNome().equals("")) {
+            rm.setMensagem("O nome do Produto é Obrigatório!");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+        } else if (pm.getMarca().equals("")) {
+            rm.setMensagem("O nome da Marca é Obrigatório!");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+        } else if (pm.getPreco().equals("")) {
+            rm.setMensagem("O Preço é Obrigatório!");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+        } else if (pm.getTamanho().equals("")) {
+            rm.setMensagem("O tamanho é Obrigatório!");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<ProdutoModel>(pr.save(pm), HttpStatus.CREATED);
+        }
+
     };
 
 }
