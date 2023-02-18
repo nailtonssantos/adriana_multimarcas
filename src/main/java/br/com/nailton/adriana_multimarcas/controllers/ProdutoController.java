d70e2ac501b62cccf60fa304124704611d7eb729
NailtonSSantos <devnailton@gmail.com>
2023-02-17 22:15:54 -0400

    10 Rota de cadastrar o produto

--- src/main/java/br/com/nailton/adriana_multimarcas/controllers/ProdutoController.java
+++ src/main/java/br/com/nailton/adriana_multimarcas/controllers/ProdutoController.java
@@ -1,7 +1,10 @@
 package br.com.nailton.adriana_multimarcas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nailton.adriana_multimarcas.model.ProdutoModel;
@@ -12,6 +15,11 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModel pm) {
+        return ps.cadastrar(pm);
+    }
 
     @GetMapping("/listar")
     public Iterable<ProdutoModel> listar() {
